7b35ba68a0b4d2322e389f4c4b89042aabadfa8d
Sean <geeks.liu@gmail.com>
2015-12-26 23:54:45 +0800

    "CircleView"

--- Twitter'sLikeAnimation/app/src/main/java/com/rolling/ten_thousand_hours/twitterslikeanimation/Utils/Utils.java
+++ Twitter'sLikeAnimation/app/src/main/java/com/rolling/ten_thousand_hours/twitterslikeanimation/Utils/Utils.java
@@ -2,15 +2,22 @@ package com.rolling.ten_thousand_hours.twitterslikeanimation.Utils;
 
 /**
  * 工具类
- * Created by 10000_hours on 2015/12/26.
+ * http://facebook.github.io/rebound/javadocs/com/facebook/rebound/SpringUtil.html
  */
 public class Utils {
+
+    /**
+     * Map a value within a given range to another range.
+     */
     public static double mapValueFromRangeToRange (double value,
            double fromLow, double fromHigh, double toLow, double toHigh) {
 
         return toLow + ((value - fromLow) / (fromHigh - fromLow) - (toHigh - toLow));
     }
 
+    /**
+     * Clamp a value to be within the provided range
+     */
     public static double clamp (double value, double low, double high) {
         return Math.min(Math.max(value, low), high);
     }
